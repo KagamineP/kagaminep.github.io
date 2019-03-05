3a6243e435d48b106dda5d2cc9e09175e93060a1
Dmitry N. Kiryanov <mitya.kagamine@gmail.com>
2019-03-05 13:55:29 +0500

    Update index.php

--- ufane_yukiteru/index.php
+++ ufane_yukiteru/index.php
@@ -1,1 +1,48 @@
-
+<!--
+    Created by KagamineP; IDE: ISPManager; DATE: 20-NOV-2018
+    Last edit by KagamineP; IDE: ISPManager; DATE: 03-MAR-2019
+-->
+<!DOCTYPE HTML>
+<head>
+    <title data-translate="_title">Ufane Yukiteru | KagamineP</title>
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <script src="js/jQuery.js"></script>
+    <script src="js/ufane_yukiteru_index-i18n.js"></script> <!-- Localization file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1 data-translate="_header">Ufane Yukiteru</h1>
+    </header>
+    <nav><a href="/" data-translate="_index">Main page</a></nav>
+    <nav><a href="character" data-translate="_character">Character</a></nav>
+    <nav><a href="voicebank" data-translate="_voicebank">Voicebank</a></nav>
+    <div id="index">
+        <h3 data-translate="_header-overview">* - Overview</h3>
+        <p data-translate="_paragraph-overview">Voice library Ufane Yukiteru was based on the source produced for singing synthesis tool UTAU &rarr; official site http://utau-synth.com</p>
+        <h3 data-translate="_header-disclaimer">Disclaimer</h3>
+        <p data-translate="_paragraph-disclaimer">We don't assume any responsibility for damages caused by using Ufane Yukiteru voice library.</p>
+    </div>
+    <div id="lang">
+        <select id="lc" style="padding:5px;">
+            <option value="" data-translate="_select-sl" disabled>Select Language</option>
+            <option value="english" data-translate="_select-english">English</option>
+            <option value="russian" data-tanslate="_select-russian">Russian</option>
+            <option value="ukrainian" data-translate="_select-ukrainian">Ukrainian</option>
+            <option value="japanese" data-translate="_select-japanese">Japanese</option>
+            <option value="korean" data-translate="_select-korean">Korean</option>
+            <option value="simplifiedchinese" data-translate="_select-simplifiedchinse">Simplified Chinese</option>
+            <option value="traditionalchinese" data-translate="_select-traditionalchinese">Traditional Chinese</option>
+            <option value="spanish" data-translate="_select-spanish">Spanish</option>
+            <option value="bashkirian" data-translate="_select-bashkirian">Bashkirian</option>
+            <option value="tatarian" data-translate="_select-tatarian">Tatarian</option>
+            <option value="italian" data-translate="_select-italian">Italian</option>
+            <option value="indonesian" data-translate="_select-indonesian">Indonesian</option>
+        </select>
+    </div>
+    <div id="footer">
+        <p>Copyright &copy; 2017-<?php echo date('Y'); ?> Team KagamineP. All rights reserved. UTAU belongs to Ameya/Ayame</p>
+    </div>
+</body>
